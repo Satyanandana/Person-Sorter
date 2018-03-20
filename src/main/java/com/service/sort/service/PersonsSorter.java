7a154301a8b9da3e96b86f3c20526e2c2b3a33f7
Satyanandana <srikanthvarma.vadapalli@gmail.com>
2018-03-20 14:03:29 -0700

    updated PersonSorter

--- src/main/java/com/service/sort/service/PersonsSorter.java
+++ src/main/java/com/service/sort/service/PersonsSorter.java
@@ -9,38 +9,31 @@ import static java.util.stream.Collectors.toList;
 
 public class PersonsSorter {
 
-    private static final Map<String, Comparator<Person>> comparatorMap = new HashMap<>();
+    private static final Map<String, Comparator<Person>> COMPARATOR_MAP = new HashMap<>();
     private static final String FALSE = "false";
     private static final String TRUE = "true";
     private static final String KEY_DELIMITER = "#";
-
     private static final String SSN = "ssn";
-
     private static final String DATEOFBIRTH = "dateofbirth";
-
     private static final String FIRSTNAME = "firstname";
-
     private static final String LASTNAME = "lastname";
-
     private static final String HEIGHTIN = "heightin";
-
     private static final String WEIGHTLB = "weightlb";
-
     private static final String DEFAULT_SORT_KEY = SSN + KEY_DELIMITER + TRUE;
 
     static {
-        comparatorMap.put(DEFAULT_SORT_KEY, Comparator.comparing(Person::getSsn));
-        comparatorMap.put(SSN + KEY_DELIMITER + FALSE, Comparator.comparing(Person::getSsn).reversed());
-        comparatorMap.put(DATEOFBIRTH + KEY_DELIMITER + TRUE, Comparator.comparing(Person::getDateOfBirth));
-        comparatorMap.put(DATEOFBIRTH + KEY_DELIMITER + FALSE, Comparator.comparing(Person::getDateOfBirth).reversed());
-        comparatorMap.put(FIRSTNAME + KEY_DELIMITER + TRUE, Comparator.comparing(Person::getFirstName));
-        comparatorMap.put(FIRSTNAME + KEY_DELIMITER + FALSE, Comparator.comparing(Person::getFirstName).reversed());
-        comparatorMap.put(LASTNAME + KEY_DELIMITER + TRUE, Comparator.comparing(Person::getLastName));
-        comparatorMap.put(LASTNAME + KEY_DELIMITER + FALSE, Comparator.comparing(Person::getLastName).reversed());
-        comparatorMap.put(HEIGHTIN + KEY_DELIMITER + TRUE, Comparator.comparing(Person::getHeightIn));
-        comparatorMap.put(HEIGHTIN + KEY_DELIMITER + FALSE, Comparator.comparing(Person::getHeightIn).reversed());
-        comparatorMap.put(WEIGHTLB + KEY_DELIMITER + TRUE, Comparator.comparing(Person::getWeightLb));
-        comparatorMap.put(WEIGHTLB + KEY_DELIMITER + FALSE, Comparator.comparing(Person::getWeightLb).reversed());
+        COMPARATOR_MAP.put(DEFAULT_SORT_KEY, Comparator.comparing(Person::getSsn));
+        COMPARATOR_MAP.put(SSN + KEY_DELIMITER + FALSE, Comparator.comparing(Person::getSsn).reversed());
+        COMPARATOR_MAP.put(DATEOFBIRTH + KEY_DELIMITER + TRUE, Comparator.comparing(Person::getDateOfBirth));
+        COMPARATOR_MAP.put(DATEOFBIRTH + KEY_DELIMITER + FALSE, Comparator.comparing(Person::getDateOfBirth).reversed());
+        COMPARATOR_MAP.put(FIRSTNAME + KEY_DELIMITER + TRUE, Comparator.comparing(Person::getFirstName));
+        COMPARATOR_MAP.put(FIRSTNAME + KEY_DELIMITER + FALSE, Comparator.comparing(Person::getFirstName).reversed());
+        COMPARATOR_MAP.put(LASTNAME + KEY_DELIMITER + TRUE, Comparator.comparing(Person::getLastName));
+        COMPARATOR_MAP.put(LASTNAME + KEY_DELIMITER + FALSE, Comparator.comparing(Person::getLastName).reversed());
+        COMPARATOR_MAP.put(HEIGHTIN + KEY_DELIMITER + TRUE, Comparator.comparing(Person::getHeightIn));
+        COMPARATOR_MAP.put(HEIGHTIN + KEY_DELIMITER + FALSE, Comparator.comparing(Person::getHeightIn).reversed());
+        COMPARATOR_MAP.put(WEIGHTLB + KEY_DELIMITER + TRUE, Comparator.comparing(Person::getWeightLb));
+        COMPARATOR_MAP.put(WEIGHTLB + KEY_DELIMITER + FALSE, Comparator.comparing(Person::getWeightLb).reversed());
     }
 
     private PersonsSorter() {
@@ -49,15 +42,17 @@ public class PersonsSorter {
 
 
     public static List<Person> sort(Iterable<Person> people, String sortField, String ascending) {
-        List<Person> personList = new ArrayList<>();
+
         if (people == null) {
-            return personList;
+            return new ArrayList<>();
         }
 
-        Comparator comparator = comparatorMap.get((sortField + KEY_DELIMITER + ascending).toLowerCase());
+        Comparator comparator = COMPARATOR_MAP.get((sortField + KEY_DELIMITER + ascending).toLowerCase());
         if (comparator == null) {
-            comparator = comparatorMap.get(DEFAULT_SORT_KEY);
+            comparator = COMPARATOR_MAP.get(DEFAULT_SORT_KEY);
         }
+
+        List<Person> personList;
 
         personList = StreamSupport.stream(people.spliterator(), false).collect(toList());
         personList.sort(comparator);
